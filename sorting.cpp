44f1824f1e7c7ad34a2b9769b65c4b9dc024d791
nhquanhcmus <quannguyenhoang79@gmail.com>
2024-06-16 22:22:39 +0700

    getRecord update func pointer

--- sorting.cpp
+++ sorting.cpp
@@ -1,12 +1,13 @@
 #include "sorting.h"
 
-Record getRecord(int a[], int n, void (*sortFunction)(int[], int, int&)) {
+Record getRecord(int a[], int n, void (*sortFunctionCmp)(int[], int, int&), void (*sortFunction)(int[], int)) {
     Record record;
     record.comparision = 0;
     record.time = 0;
+    sortFunctionCmp(a, n, record.comparision);
     clock_t start, end;
     start = clock();
-    sortFunction(a, n, record.comparision);
+    sortFunction(a, n);
     end = clock();
     record.time = (end - start) * 1000 / CLOCKS_PER_SEC;
     return record;
