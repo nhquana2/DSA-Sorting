e570b839a0bd93bfedfbfbeb710e228cb088ca71
To My <tomy02032005@gmail.com>
2024-06-18 00:19:03 +0700

    insertion sort

---
+++ insertionsort.cpp
@@ -0,0 +1,38 @@
+#include "sorting.h"
+
+// Original function
+void insertionSort(int a[], int n) {
+    int i, j, key;
+    for (i = 1; i < n; i++) 
+    {
+        key = a[i];
+        j = i - 1;
+
+        while (j >= 0 && a[j] > key) 
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
+
+// Function with comparision counter
+void insertionSort(int a[], int n, int &comparision) {
+    comparision = 0;
+    
+    int i, j, key;
+	for (i = 1; ++comparision && i < n; i++)
+	{
+		key = a[i];
+		j = i - 1;
+
+		while (++comparision && j >= 0 && a[j] > key)
+		{
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = key;
+	}
+}
+
