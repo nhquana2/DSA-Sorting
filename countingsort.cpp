b9e6f88b7582dae57bc28b711af9ae760e0c9c22
Thái Hoàng Phúc <thaihoangphuc7556@gmail.com>
2024-06-21 22:48:54 +0700

    Counting Sort (Modified)
    + Modify to sort negative values

--- countingsort.cpp
+++ countingsort.cpp
@@ -2,54 +2,62 @@
 using namespace std;
 
 void countingSort(int a[], int n) {
-    int maxx = -1;
+    int maxx = INT_MIN;
+    int minn = INT_MAX;
     for (int i = 0; i < n; i++) {
         maxx = max(maxx , a[i]);
+        minn = min(minn, a[i]);
     }
-    int *c = new int[maxx + 1];
-    for (int i = 0; i < maxx; i++) {
+    int range = maxx - minn + 1;
+    int *c = new int[range + 1];
+    for (int i = 0; i < range; i++) {
         c[i] = 0;
     }
     for (int i = 0; i < n; i++) {
-        c[a[i]]++;
+        c[a[i] - minn]++;
     }
-    for (int i = 1; i < maxx + 1; i++) {
+    for (int i = 1; i < range; i++) {
         c[i] += c[i - 1];
     }
     int *o = new int[n];
     for (int i = n - 1; i >= 0; i--) {
-		o[c[a[i]] - 1] = a[i];
-		c[a[i]]--;
-	}
-	for (int i = 0; i < n; i++) {
-		a[i] = o[i];
-	}
+        o[c[a[i] - minn] - 1] = a[i];
+        c[a[i] - minn]--;
+    }
+    for (int i = 0; i < n; i++) {
+        a[i] = o[i];
+    }
     delete[] c;
+    delete[] o;
 }
 
 void countingSort(int a[], int n, int& comparison) {
     comparison = 0;
-    int maxx = -1;
+    int maxx = INT_MIN;
+    int minn = INT_MAX;
     for (int i = 0; ++comparison && i < n; i++) {
         maxx = max(maxx , a[i]);
+        minn = min(minn, a[i]);
     }
-    int *c = new int[maxx + 1];
-    for (int i = 0; ++comparison && i < maxx; i++) {
+    int range = maxx - minn + 1;
+    int *c = new int[range + 1];
+    for (int i = 0; ++comparison && i < range; i++) {
         c[i] = 0;
     }
     for (int i = 0; ++comparison && i < n; i++) {
-        c[a[i]]++;
+        c[a[i] - minn]++;
     }
-    for (int i = 1; ++comparison && i < maxx + 1; i++) {
+    for (int i = 1; ++comparison && i < range; i++) {
         c[i] += c[i - 1];
     }
     int *o = new int[n];
     for (int i = n - 1; ++comparison && i >= 0; i--) {
-		o[c[a[i]] - 1] = a[i];
-		c[a[i]]--;
-	}
-	for (int i = 0; ++comparison && i < n; i++) {
-		a[i] = o[i];
-	}
+        o[c[a[i] - minn] - 1] = a[i];
+        c[a[i] - minn]--;
+    }
+    for (int i = 0; ++comparison && i < n; i++) {
+        a[i] = o[i];
+    }
     delete[] c;
+    delete[] o;
 }
